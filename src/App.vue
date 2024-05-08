7565b1375ae0d4f1aff13cda3752fe76f48e0d5d
Tri Le Nguyen <tri.lenguyenhuu@atg.co.uk>
2024-05-08 13:16:04 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@ import Modal from './components/Modal.vue'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
   
   <Modal />
 </template>
