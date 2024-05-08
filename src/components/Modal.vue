9936c985ada7b9ad767bcf5e2e396d61072f518f
trilnh92 <trilnh92@gmail.com>
2024-05-08 13:43:44 +0700

    Update Model Comp

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+  name: String
+})
+
+// const name = ref("active");
+
+function handleClick() {
+    console.log(this.$refs.name);
+    this.$refs.name.classList.add('active');
+}
 
 </script>
 
 <template>
-
+<h1>{{ msg }}</h1>
+<input type="text" ref="name" />
+<button @click="handleClick">Click Me</button>
 </template>
 
 <style scoped>
